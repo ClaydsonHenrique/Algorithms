ec8a5e2592591523849a8d362adbe72d8b7549f6
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-02-07 16:29:18 -0300

    finalizando requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    time = 0
+
+    for Students, Students2 in permanence_period:
+        if not isinstance(Students, int) or not isinstance(Students2, int):
+            return None
+        if Students <= target_time <= Students2:
+            time += 1
+
+    return time
