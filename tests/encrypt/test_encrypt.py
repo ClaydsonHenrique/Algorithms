d138e7b7a7ec60fa69aad80a908f5fcffff329c7
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-02-07 17:04:40 -0300

    fix: ajustando erro requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,10 +3,6 @@ import pytest
 
 
 def test_encrypt_message():
-
-    with pytest.raises(TypeError):
-        encrypt_message("message", "key")
-
     assert encrypt_message("message", 0) == "egassem"
 
     assert encrypt_message("message", 3) == "egas_sem"
@@ -14,3 +10,6 @@ def test_encrypt_message():
     assert encrypt_message("message", 2) == "ssemeg_am"
 
     assert encrypt_message("message", 4) == "mseg_essa"
+
+    with pytest.raises(TypeError):
+        encrypt_message("message", "key")
