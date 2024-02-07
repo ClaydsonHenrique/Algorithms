4be78e67d7c4f63e0626182a8aff0ae50f8e5448
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-02-07 16:43:48 -0300

    finalizando requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,23 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+
+    with pytest.raises(TypeError):
+        encrypt_message("message", "key")
+
+    with pytest.raises(TypeError):
+        encrypt_message(123, 4)
+
+    assert encrypt_message("message", 0) == "egassem"
+
+    assert encrypt_message("message", 7) == "egassem"
+
+    assert encrypt_message("message", 3) == "ssemeg_am"
+
+    assert encrypt_message("message", 4) == "mseg_essa"
+
+    assert encrypt_message("hello_world", 6) == "dlo_worlh_el"
+
+    assert encrypt_message("hello_world", 5) == "o_dlohl_wrld"
